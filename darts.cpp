d33f45fdc649836c7c922e4c202d601009af8ea8
Tess Huelskamp <tess.huelskamp@gmail.com>
2017-12-10 20:43:01 -0700

    outline

---
+++ darts.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using std::cout; using std::endl;
+
+
+/*
+ * 538 dart riddler December 2018
+ *
+ * How many ways are there to win a perfect game of darts?
+ *
+ * sounds susp like a problem we'd get in 831 so I thought I would
+ * code it out as a recursive solution and then figure out how to
+ * do it using dynamic programming :D
+ *
+ * not a bad way to spend a Sunday
+ *
+ */
+
+
+int dartsRec(int toScore){
+  static vector<int> board;
+
+}
+int dartsDyPro(int toScore){
+
+}
+
+
+int main(){
+
+}
