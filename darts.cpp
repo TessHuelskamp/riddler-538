ca3090e0517035b104594aaf0c0ef5de516c3985
Tess Huelskamp <tess.huelskamp@gmail.com>
2017-12-11 10:32:44 -0700

    add dypro alg in

--- darts.cpp
+++ darts.cpp
@@ -6,7 +6,6 @@ using std::find;
 
 #include <vector>
 using std::vector;
-
 #include <map>
 using std::map;
 
@@ -33,16 +32,24 @@ private:
   unsigned long long internalCalc(int, int);
   unsigned long long cacheOrCalc(int, int);
   void makeBoard();
+  pair<int,int> makePair(int points, int turns){pair<int,int> result(points,turns); return result;};
+
+  //board is all of the possible moves we could make
+  //note that some numbers are double (or triply!) counted due to how
+  //I decided to count "different" ways of throwing a dart
   vector<int> board;
+
+  //moves that are only available on the last turn
   vector<int> lastTurn;
 
-  const int numPoints=501;
+  const int maxPoints=501;
   const int maxTurns=11;
 
 
 public:
   unsigned long long calculate();
-  Dart(int np,int t) : numPoints(np), maxTurns(t) {}
+  unsigned long long dypro();
+  Dart(int mp,int t) : maxPoints(mp), maxTurns(t) {}
   Dart(){};
 
 };
@@ -51,10 +58,8 @@ public:
 unsigned long long Dart::calculate(){
   makeBoard();
 
-  // *a* perfect game
-  // 9*50 + 1 * 50 // need to end with a bulls eye or double
-  cacheOrCalc(numPoints,maxTurns);
-  pair<int,int> key(numPoints,maxTurns);
+  cacheOrCalc(maxPoints,maxTurns);
+  pair<int,int> key(maxPoints,maxTurns);
   return cache[key];
 }
 
@@ -113,17 +118,72 @@ unsigned long long Dart::internalCalc(int toScore, int turns){
     }
     return totalPossibleWins;
   }
+}
 
+unsigned long long Dart::dypro(){
+  cache.clear();
+
+  //interface
+  pair<int,int> key;
+
+  // mark all of the games that can be won on the last turn
+  // I think this is fair to assume that the cache will be inited to zeros
+  // There shouldn't be duplicate ways to win a game here
+  // (all of the last winning moves have a unique number of points)
+  for ( const int point : lastTurn){
+    key=makePair(point,1);
+    cache[key]++;
+  }
+
+  for (int turn=2; turn<=maxTurns; ++turn){
+    for (int point=1; point<=maxPoints; ++point){
+      //check all possible games starting from the ground up
+      unsigned long long possibleWins=0;
+
+      //check all possible moves we can make
+      for (const int move : board){
+        //check to see if score would put us at or below 0
+        //both cases would be an invalid game
+        if (point-move >0){
+
+          //check to see if that game is a previously winning game
+          key=makePair(point-move,turn-1);
+          auto it = cache.find(key);
+          if (it != cache.end()){
+            possibleWins+=cache[key];
+          }
+        }
+      }
+
+      //cache entry if bigger than 0
+      //I only want to score winable games
+      if (possibleWins > 0){
+        key=makePair(point,turn);
+        cache[key]=possibleWins;
+      }
+
+    }
+  }
+
+  //check to see if there's a way to win this board
+  unsigned long long result=0;
+  auto it=cache.find(makePair(maxPoints, maxTurns));
+  if (it!=cache.end()) result=cache[makePair(maxPoints, maxTurns)];
+  return result;
 }
 
 int main(){
   Dart d;
   cout << d.calculate() << endl;
+  cout << d.dypro() << endl;
 
-  Dart e(50,2);
+  Dart e(121,2);
   cout << e.calculate() << endl;
 
   Dart f(50,1);
   cout << f.calculate() << endl;
+
+  cout << endl << endl;
+  cout << "should not work" << e.dypro() << endl;
 
 }
