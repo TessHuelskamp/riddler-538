e5a4aa5d35031cce75da07c0e6f32f463e8a2281
Tess Huelskamp <tess.huelskamp@gmail.com>
2018-06-17 19:24:44 -0700

    clean up code

--- concentration/concentration.py
+++ concentration/concentration.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 
-import random
+from random import sample
 
-NUM_TESTS=100
+NUM_TESTS=10000
 PAIRS=10
 
 numToChr=lambda n : chr(ord("A")+n)
 
-totalTime=0
+# I intended to build a histogram here but ran out of time
+allGames=list()
+
 for _ in range(NUM_TESTS):
 
     # build board
@@ -16,25 +18,27 @@ for _ in range(NUM_TESTS):
         board.add(numToChr(i)+"0")
         board.add(numToChr(i)+"1")
 
-
     # play game
-    time=0
+    gameDuration=0
     while board:
 
         # flip over two tiles and wait for cards to flip over/make animal noise
-        # Happens in both success and failure
-        time+=3
+        # Happens in success and failure
+        gameDuration+=3
 
-        first, second = random.sample(board, 2)
+        # set.pop() returns an _aribitary_ element not a random one.
+        # flip over two tiles
+        first, second = sample(board, 2)
 
-        # remove elements if they are a match
+        # remove tiles if they are a match
+        # (they are implictly "flipped over" if we do nothing).
         if first[0] == second[0]:
             board.remove(first)
             board.remove(second)
 
-    # could have kept a running total and divided at end but this make more sense to me
-    totalTime+=time
+    # keep running total of all games
+    allGames.append(gameDuration)
 
-# print avg
-print(totalTime/float(NUM_TESTS))
-
+# pls don't divide by 0 :)
+average = sum(allGames)/len(allGames)
+print(average)
